b09157864cb8b811726cfc712c2890ff28a1426f
5im <simon@uufstellend.ch>
2016-11-02 23:03:24 +0100

    Fixed HelloControllerIT (wrong import -.-)

--- src/main/java/ch/ffhs/innt/eibacon/hello/Application.java
+++ src/main/java/ch/ffhs/innt/eibacon/hello/Application.java
@@ -6,9 +6,11 @@
 package ch.ffhs.innt.eibacon.hello;
 
 import java.util.Arrays;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Hello world application.
@@ -19,14 +21,21 @@ import org.springframework.context.ApplicationContext;
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
+    }
 
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
 
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
     }
 }
