dd256fbe496ac03f08be8b6115bbefd56474f8d1
5im <simon@uufstellend.ch>
2016-11-03 23:39:34 +0100

    Fixed war deployment

--- src/main/java/ch/ffhs/innt/eibacon/hello/Application.java
+++ src/main/java/ch/ffhs/innt/eibacon/hello/Application.java
@@ -5,12 +5,10 @@
  */
 package ch.ffhs.innt.eibacon.hello;
 
-import java.util.Arrays;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Hello world application.
@@ -18,24 +16,14 @@ import org.springframework.context.annotation.Bean;
  * @author Simon
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
-        };
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
     }
 }
