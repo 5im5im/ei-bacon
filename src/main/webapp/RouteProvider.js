2abd36483c87bd26c7812868d620eb6442e6f7d3
5im <simon@uufstellend.ch>
2016-12-29 00:34:37 +0100

    Added AngularJS and its navigation

---
+++ src/main/webapp/RouteProvider.js
@@ -0,0 +1,25 @@
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function ($routeProvider) {
+    $routeProvider
+            .when("/", {
+                templateUrl: "main/main.html"
+            })
+            .when("/einfahrt", {
+                templateUrl: "einfahrt/einfahrt.html"
+            })
+            .when("/ticket", {
+                templateUrl: "ticket/ticket.html"
+            })
+            .when("/ausfahrt", {
+                templateUrl: "ausfahrt/ausfahrt.html"
+            })
+            .when("/account", {
+                templateUrl: "account/account.html"
+            })
+            .when("/admin", {
+                templateUrl: "admin/index.html"
+            })
+            .otherwise({
+                template: "<h2>Error 404</h2><p>Seite nicht gefunden.</p>"
+            });
+});
