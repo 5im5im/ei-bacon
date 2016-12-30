8a806de95d526e35db802dbeeae7a9a4937d131b
5im <simon@uufstellend.ch>
2016-12-30 20:39:18 +0100

    More explicit parameter naming - fix

--- src/main/webapp/ParkingServiceRestletCtrl.js
+++ src/main/webapp/ParkingServiceRestletCtrl.js
@@ -4,7 +4,7 @@ app.controller('parkingServiceRestletCtrl', function ($scope, $http) {
             method: 'POST',
             url: 'parkingService/startParking',
             params: {
-                "entryBeaconId": $scope.beaconId,
+                "entryBeaconId": $scope.entryBeaconId,
                 "accountId": $scope.accountId
             },
             headers: {'Content-Type': 'application/x-www-form-urlencoded'}
@@ -13,7 +13,7 @@ app.controller('parkingServiceRestletCtrl', function ($scope, $http) {
         }, function onError(response) {
             console.log(response.status)
             console.log(response.data)
-            console.log("beaconId" + $scope.beaconId)
+            console.log("entryBeaconId" + $scope.entryBeaconId)
             console.log("accountId" + $scope.accountId)
             $scope.errMsg = "parkingService/startParking failed with status " + response.status;
         });
@@ -24,7 +24,7 @@ app.controller('parkingServiceRestletCtrl', function ($scope, $http) {
             method: 'POST',
             url: 'parkingService/endParking',
             params: {
-                "exitBeaconId": $scope.beaconId,
+                "exitBeaconId": $scope.exitBeaconId,
                 "ticketId": $scope.ticketId
             },
             headers: {'Content-Type': 'application/x-www-form-urlencoded'}
@@ -33,7 +33,7 @@ app.controller('parkingServiceRestletCtrl', function ($scope, $http) {
         }, function onError(response) {
             console.log(response.status)
             console.log(response.data)
-            console.log("beaconId" + $scope.beaconId)
+            console.log("exitBeaconId" + $scope.exitBeaconId)
             console.log("ticketId" + $scope.ticketId)
             $scope.errMsg = "parkingService/endParking failed with status " + response.status;
         });
